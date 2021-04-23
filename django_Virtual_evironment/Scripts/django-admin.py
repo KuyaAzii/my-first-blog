be41616463af13518bd144c7ff1533903456bdfb
ace <ace@ace.ace>
2021-04-23 13:39:01 +0800

    My Django Girls app, first commit

---
+++ django_Virtual_evironment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\acedc\djangogirls\django_virtual_evironment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
